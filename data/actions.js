f90cb1a4175432878c174bc5323c87232e5087e8
badCompany55 <zachirvin45@gmail.com>
2019-03-21 21:55:36 -0500

    GET ENDPOINTS add dishes endpoint add single dish endpoint add recipies endpoint

--- data/actions.js
+++ data/actions.js
@@ -1,1 +1,21 @@
 const db = require("./knex.js");
+
+module.exports = {
+  all_dishes,
+  a_dish,
+  all_recipes
+};
+
+function all_dishes() {
+  return db("dishes");
+}
+
+function a_dish(id) {
+  return db("dishes").where("id", id);
+}
+
+function all_recipes() {
+  return db("dishes")
+    .select("rec_name", "dish_name")
+    .innerJoin("recipes", "dis_rec_id", "recipes.id");
+}
--- routes/cookbook.js
+++ routes/cookbook.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const db = require("../data/actions.js");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("testing");
+router.get("/dishes", async (req, res) => {
+  try {
+    const dishes = await db.all_dishes();
+    res.status(200).json(dishes);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/dishes/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const dish = await db.a_dish(id);
+    dish.length > 0
+      ? res.status(200).json(dish)
+      : res.status(400).json({ Error: "Invalid dish" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/recipes", async (req, res) => {
+  try {
+    const recipes = await db.all_recipes();
+    res.status(200).json(recipes);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
